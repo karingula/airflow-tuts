67607ce05d292dfe8742f7ec9edc1ec93fce4a98
Karingula <karingula.v@gmail.com>
2019-10-31 13:40:30 -0400

    FERNET_KEY issues

--- docker-airflow/fernet_key_set.py
+++
@@ -1,5 +0,0 @@
-from cryptography.fernet import Fernet
-import os
-
-FERNET_KEY = Fernet.generate_key().decode()
-print(FERNET_KEY)
---
+++ docker-airflow/fernet_script.sh
@@ -0,0 +1,1 @@
+export FERNET_KEY=`python -c 'from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)'`
